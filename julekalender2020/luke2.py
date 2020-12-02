7ba6040d9813507512dc199ca626c10eb090c038
Kristian Brandser <kristianbrandser@kristiansmbp508.lan>
2020-12-02 15:14:25 +0100

    some updates luke2

--- julekalender2020/luke2.py
+++ julekalender2020/luke2.py
@@ -1,23 +1,30 @@
 import numpy, julekalender2020
 
-allepakker = numpy.arange(0,10,1,dtype=int)
+#allepakker = numpy.arange(0,20,1,dtype=int)
 # 5433000
 # mask med alle pakker som skal kastes, 
 # dvs. iterere gjennom array og ved siffer 7 så 
 # skal pakken kastes + antall nærmeste primtall, 
 # som er mindre eller lik pakkenummeret
+allepakker = list(range(0,10000,1))
 kastedepakker = []
-for pakke in allepakker:
+lastPrime = 0
 
-    if (julekalender2020.isPrime(pakke)):
-        lastPrime = pakke
+for pakke in allepakker:
+    
+    if (pakke not in kastedepakker):
+        if (julekalender2020.isPrime(pakke)):
+            lastPrime = pakke
 
     if (str('7') in str(pakke)):
         # Santa sad
-        kastedepakker.extend(range(pakke, pakke+lastPrime+1))
+        kastedepakker.extend(allepakker[pakke:pakke+lastPrime+1])
       
 levertepakker = numpy.setdiff1d(allepakker, kastedepakker)
-print("Antall leverte pakker: ", numpy.size(levertepakker))
 
-print("Kastede pakker")
-print(kastedepakker)
+print("Antall leverte pakker: ", len(levertepakker))
+print(levertepakker)
+print("Totalt antall pakker: ", len(allepakker))
+print("Antall kastede pakker: ", len(kastedepakker))
+#print(kastedepakker)
+
